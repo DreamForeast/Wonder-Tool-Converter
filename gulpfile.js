4364b71a73610dcacc88228e986d85f234b19162
yyc <395976266@qq.com>
2018-06-23 21:39:23 +0800

    fix: fix gulpfile->convert gulp task bug; update wonder.js version

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ gulp.task("convert", function (done) {
     var sourceGLTFFilePath = commandUtils.parseOption("--sourceGLTFFilePath"),
         destDir = commandUtils.parseOption("--destDir") || "./dest/";
 
-    if (sourceDir === null) {
+    if (destDir === null) {
         throw new Error("should define --sourceGLTFFilePath");
     }
 
